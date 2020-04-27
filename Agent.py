e8cbf6fdd36efee1cde68afe212547bbed3a6a8d
blackmange26 <edwardwang@college.harvard.edu>
2020-04-26 21:57:44 -0400

    Shop implemented for each agent

--- Agent.py
+++ Agent.py
@@ -8,13 +8,17 @@ Created on Sat Apr 25 23:56:44 2020
 #ToDo: Create a class that holds the board and bench for every player
 
 import copy
+import random
+from ChampionLoader import ChampionLoader
 
 class Agent(object):
-    def __init__(self, name, champion, gold):
+    def __init__(self, name, champion, gold, champion_loader = ChampionLoader()):
         self.champion = champion
         self.board = {
         }
         self.gold = gold
+        self.champion_loader = champion_loader
+        self.shop = random.sample(list(self.champion_loader.data.keys()), 5)
 
     def getChamp(self):
         return self.champion
--- Test.py
+++ Test.py
@@ -7,9 +7,12 @@ Created on Sat Apr 25 23:11:29 2020
 
 from ChampionLoader import ChampionLoader
 from Game import Game
+from Agent import Agent
 
-Game = Game(ChampionLoader(), 'Aatrox', 'Fiora')
-print(Game.run())
+Agent = Agent ('Eddie', 'Aatrox', 5).shop
+print(Agent)
+# Game = Game(ChampionLoader(), 'Aatrox', 'Fiora')
+# print(Game.run())
 # print(Game.r)
 # Champ_Object = ChampionLoader()
 # print(Champ_Object.getChampion('Jinx'))
